00c4726ef4a800ae6e27713dd484d51481ea2eed
Willi Schlegel <schlegel.willi@yahoo.com>
2025-07-24 10:18:43 +0200

    initial commit

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,11 +4,11 @@
 #include "UI/ui.h"
 
 
-int  WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
-	int       nShowCmd
+int WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nShowCmd
 )
 {
 	UI->drawSelection();
